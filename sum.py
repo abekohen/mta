7ea8b9b5f3814b40b0dd08fb3751272518b79534
Erik Bernhardsson <mail@erikbern.com>
2016-03-03 11:02:09 -0500

    moar graphs

--- sum.py
+++ sum.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 import seaborn
+import numpy
 from matplotlib import pyplot
 
 stations = {}
@@ -21,6 +22,24 @@ for key, values in stations.iteritems():
             deltas.append(value - last_value)
         last_value = value
 
-seaborn.distplot(deltas)
-pyplot.savefig('deltas.png')
-# pyplot.show()
+print 'got', len(deltas), 'deltas'
+seaborn.distplot([d for d in deltas if d < 3600])
+pyplot.savefig('time_between_arrivals.png')
+
+percs = [50, 75, 90, 95, 97, 98, 99]
+results = [[] for perc in percs]
+offsets = range(0, 20 * 60)
+for offset in offsets:
+    print offset, '...'
+    rs = numpy.percentile([d-offset for d in deltas if d >= offset], percs)
+    for i, r in enumerate(rs):
+        results[i].append(r)
+
+pyplot.clf()
+for i, result in enumerate(results):
+    pyplot.plot(offsets, result, label='%d percentile' % percs[i])
+pyplot.ylim([0, max(offsets)*3])
+pyplot.xlabel('Time you have waited for the subway')
+pyplot.ylabel('Additional time until subway arrives')
+pyplot.legend()
+pyplot.savefig('time_until_arrival.png')

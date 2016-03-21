463007805d682dc3dc38c489a0ffa2f4fda91f60
Erik Bernhardsson <erikbern@spotify.com>
2016-03-20 22:25:24 -0400

    plotting trips too

---
+++ plot_trips.py
@@ -0,0 +1,44 @@
+import datetime
+import json
+import matplotlib.pyplot as plt
+import pytz
+
+# nyc_tz = pytz.timezone('US/Eastern')
+
+def read_data():
+    date_a, date_b = (int(dt.strftime('%s')) for dt in [datetime.datetime(2016, 3, 3, 14), datetime.datetime(2016, 3, 3, 20)])
+    trips = {}
+    for line in open('log.jsons'):
+        for vehicle in json.loads(line.strip()):
+            if vehicle.get('current_status') != 1: # STOPPED_AT
+                continue
+        
+            if vehicle['trip']['route_id'][:1] != '1':
+                continue
+
+            t = vehicle['timestamp']
+                        
+            if t > date_b:
+                return trips
+
+            if t < date_a or t > date_b:
+                continue
+        
+            stop = vehicle['stop_id']
+            trip = (vehicle['trip']['start_date'], vehicle['trip']['trip_id'])
+            trips.setdefault(trip, {})[t] = stop
+
+
+plt.figure(figsize=(24, 6))
+for trip, seq in read_data().iteritems():
+    if len(seq) < 10:
+        continue
+    seq = sorted(seq.items())
+    ts = [datetime.datetime.utcfromtimestamp(t) for t, _ in seq]
+    ys = [int(s[:-1]) for _, s in seq]
+    plt.plot_date(ts, ys, '#00933C') #, tz=nyc_tz)
+
+plt.tight_layout()    
+plt.savefig('trips.png')
+
+        
